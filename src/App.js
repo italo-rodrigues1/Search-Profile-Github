c71d535aab4b966f9fc4b75af36e28cb03d162ac
italo-rodrigues1 <italosport110@hotmal.com>
2020-12-24 18:00:13 -0300

    1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,70 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import GithubImage from './github-icon.svg';
 import './App.css';
 
 function App() {
+
+  const [search,setSearch] = useState('');
+  const [userData,setUserData] = useState();
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    fetch(`https://api.github.com/users/${search}`)
+      .then(response => response.json())
+      .then(userResponse => setUserData(userResponse));
+  }
+
+  const handleChange = (event) => {
+    setSearch(event.target.value);
+  }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container text-center">
+      <h1 id="title" className="py-5 text-uppercase">
+        perfil do github
+      </h1>
+      <form onSubmit={handleSubmit}>
+        <div className="form-group">
+          <div className="input-group">
+            <input type="text" placeholder="Digite o perfil do github desejado..." value={search} onChange={handleChange} require/>
+
+           <span className="input-group-btn">
+             <button type="submit" id="btn-procurar">
+               Procurar
+             </button>
+           </span>
+          </div>
+        </div>
+      </form>
+      
+      <div className="py-5">
+        
+        {!userData && (
+          <img src={GithubImage} className ="responsive rounded-circle" alt="logo" height="200px"/>
+        )}
+
+        {userData &&(
+          <div>
+           <img src={userData.avatar_url} className ="responsive rounded-circle" alt="logo" height="200px"/>
+          
+          <h1 className="pt-5">
+            <a href={userData.html_url} target="_new">
+              {userData.name}
+            </a>
+          </h1>
+          <h3>
+            {userData.location}
+          </h3>
+
+          <p id="camp-bio" className="text-info">
+            {userData.bio}
+          </p>
+
+          </div>
+        )}
+        
+      </div>
     </div>
   );
 }
